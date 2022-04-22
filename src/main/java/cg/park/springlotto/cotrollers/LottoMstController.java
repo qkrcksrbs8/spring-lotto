62921104d43cf6b6bfe9ff11b7b1bc9897636b45
changyun <qkrcksrbs8@naver.com>
2022-04-22 20:59:08 +0900

    로또번호 역대 당첨 수 조회 퍼센트 api 구현

--- src/main/java/cg/park/springlotto/cotrollers/LottoMstController.java
+++ src/main/java/cg/park/springlotto/cotrollers/LottoMstController.java
@@ -1,6 +1,10 @@
 package cg.park.springlotto.cotrollers;
 
+import cg.park.springlotto.models.MessageDto;
+import cg.park.springlotto.services.impl.LottoMstServiceImpl;
+import cg.park.springlotto.utils.LottoHistoryUtil;
 import cg.park.springlotto.utils.Param;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,14 +15,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class LottoMstController {
 
+    @Autowired
+    LottoHistoryUtil historyUtil;
+
+    @Autowired
+    LottoMstServiceImpl lottoMstService;
+
     @GetMapping("/thisWeek")
     public Param thisWeek() {
         return new Param();
     }
 
     @GetMapping("/history")
-    public Param historyList() {
-        return new Param();
+    public MessageDto historyList() {
+        return historyUtil.resProxy(lottoMstService.execute(new Param()));
     }
 
 }
--- src/main/java/cg/park/springlotto/services/impl/LottoMstServiceImpl.java
+++ src/main/java/cg/park/springlotto/services/impl/LottoMstServiceImpl.java
@@ -1,4 +1,28 @@
 package cg.park.springlotto.services.impl;
 
-public class LottoMstServiceImpl {
+import cg.park.springlotto.daos.HistoryDao;
+import cg.park.springlotto.models.HistoryCountDto;
+import cg.park.springlotto.services.Command;
+import cg.park.springlotto.utils.Param;
+import cg.park.springlotto.utils.PcgUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LottoMstServiceImpl extends Command {
+
+    @Autowired
+    PcgUtil pcgUtil;
+
+    @Autowired
+    HistoryDao historyDao;
+
+    public Param pre(Param param) {
+        return new Param().set("code", pcgUtil.toEnum("S0001"));
+    }
+
+    public Param post(Param param) {
+        HistoryCountDto historyCount = historyDao.selectHistoryCount();
+        return new Param().set("code", pcgUtil.toEnum("S0001")).set("lottoPercentage", historyCount);
+    }
 }
